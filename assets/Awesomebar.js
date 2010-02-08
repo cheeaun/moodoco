ee6d98bff85c8165b48bb4b69349f16b42a7b02b
Lim Chee Aun <cheeaun@gmail.com>
2010-02-08 22:21:21 +0800

    More awesomebar magic.

--- assets/Awesomebar.js
+++ assets/Awesomebar.js
@@ -113,14 +113,17 @@ var Awesomebar = new Class({
 						return;
 					}
 					if (value == prevValue) return;
-					prevValue = value;
-					value = value.escapeRegExp().replace(' ', '|')
+					prevValue = value = value.escapeRegExp();
+					value = value.replace(/\s+/g, '.*')
 					var fdata = self.data.filter(function(d){
-						return d.primary.test(value, 'i') || d.secondary.test(value, 'i');
+						return d.primary.test(value, 'i');
 					});
-					var found = true;
 					var html = '';
-					if (fdata.length){
+					var fdatalen = fdata.length;
+					var limit = options.resultsLimit;
+					
+					if (fdatalen){
+						var v = prevValue.replace(/\s+/g, '|');
 						var pattern = new RegExp(value, 'ig');
 						fdata.sort(function(a, b){
 							var ap = $splat(a.primary.match(pattern)).length;
@@ -129,10 +132,11 @@ var Awesomebar = new Class({
 							if (bap != 0) return bap;
 							var as = $splat(a.secondary.match(pattern)).length;
 							var bs = $splat(b.secondary.match(pattern)).length;
-							return (bs - as);
+							return bs - as;
 						});
-						fdata = fdata.slice(0, options.resultsLimit);
-						var pat = new RegExp('(' + value + ')', 'ig');
+						fdata = fdata.slice(0, limit);
+						fdatalen = fdata.length;
+						var pat = new RegExp('(' + v + ')', 'ig');
 						var rep = '<span>$1</span>';
 						fdata.each(function(data){
 							html += '<li><a href="' + data.url + '">'
@@ -140,10 +144,57 @@ var Awesomebar = new Class({
 								+ '<span class="secondary">' + data.secondary.replace(pat, rep) + '</span>'
 							+ '</a></li>';
 						});
-					} else {
-						html += '<li class="nada">Oops, nothing found.</li>';
-						found = false;
 					}
+					
+					if (fdatalen < limit){
+						var v = prevValue.replace(/\s+/g, '|').split('|').map(function(str){
+							return '(' + str.split('').join(')[\\S]*(') + ')';
+						});
+						var v1 = v.join('.*');
+						var v2 = v.join('|');
+						var pat1 = new RegExp(v1, 'ig');
+						var pat2 = new RegExp(v2, 'ig');
+						fdata = self.data.filter(function(d){
+							return !(d.primary.test(value, 'i'));
+						}).filter(function(d){
+							return d.primary.test(v2, 'i') || d.secondary.test(v2, 'i');
+						});
+						fdata.sort(function(a, b){
+							var ap1 = $splat(a.primary.match(pat1)).length;
+							var bp1 = $splat(b.primary.match(pat1)).length;
+							var bap1 = bp1 - ap1;
+							if (bap1 != 0) return bap1;
+							var as1 = $splat(a.secondary.match(pat1)).length;
+							var bs1 = $splat(b.secondary.match(pat1)).length;
+							var bas1 = bs1 - as1;
+							if (bas1 != 0) return bas1;
+							var ap2 = $splat(a.primary.match(pat2)).length;
+							var bp2 = $splat(b.primary.match(pat2)).length;
+							var bap2 = bp2 - ap2;
+							if (bap2 != 0) return bap2;
+							var as2 = $splat(a.secondary.match(pat2)).length;
+							var bs2 = $splat(b.secondary.match(pat2)).length;
+							return (bs2 - as2);
+						});
+						fdata = fdata.slice(0, limit - fdatalen);
+						fdatalen = fdata.length;
+						var rep = function(match){
+							var s = $A(arguments).slice(1, -2).filter(function(c){return !!c}).join('|');
+							var p = new RegExp(s, 'ig');
+							return match.replace(p, function(m){return '<span>' + m + '</span>';});
+						};
+						fdata.each(function(data, i){
+							html += '<li><a href="' + data.url + '">'
+								+ '<span class="primary">' + data.primary.replace(pat2, rep) + '</span>'
+								+ '<span class="secondary">' + data.secondary.replace(pat2, rep) + '</span>'
+							+ '</a></li>';
+						});
+					}
+					
+					if (!fdatalen){
+						html += '<li class="nada">Oops, nothing found.</li>';
+					}
+					
 					list.set('html', html);
 					var firstLink = list.getElement('li:first-child a');
 					if (firstLink) firstLink.addClass('selected');
@@ -172,7 +223,8 @@ var Awesomebar = new Class({
 	},
 	
 	feed: function(data){
-		this.data = data;
+		if ($type(data) != 'array') return;
+		this.data = data.slice();
 		return this;
 	},
 	
